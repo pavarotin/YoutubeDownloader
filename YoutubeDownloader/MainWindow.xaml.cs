6a21eb7f44b2e1586262a441d17d669eef1f6138
pavarotin <knopstany228@gmail.com>
2021-04-26 16:46:02 +0300

    alpha 0.0.1

--- YoutubeDownloader/MainWindow.xaml.cs
+++ YoutubeDownloader/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YoutubeExplode;
 
 namespace YoutubeDownloader
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        YoutubeClient youtube = new YoutubeClient();
+        string url = "";
+        
+        
+      
+        private async Task GetInfo()
+        {
+           // qual;
+            var video = await youtube.Videos.GetAsync(url);
+
+            title.Content = video.Title; // "Collections - Blender 2.80 Fundamentals"
+                                         //  var author = video.Author.Title;  "Blender" size nyjen
+            duration.Content = video.Duration.Value;  // 00:07:20
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            var streamInfo = streamManifest.GetMuxedStreams();
+            foreach(var vid in streamInfo)
+            {
+                qual.Items.Add(vid.Size);
+            }
+            
+            Console.WriteLine(streamManifest);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            url = url_Box.Text;
+            GetInfo();
         }
     }
 }
